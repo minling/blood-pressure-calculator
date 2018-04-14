6cd99561d3475fe6302fe4777ec84b5f8894f565
minling <mzhao725@gmail.com>
2018-04-14 02:30:39 -0400

    puts javascript and css file into its own folder, adds logic for calculating blood pressure levels, color is dependent in result

---
+++ javascripts/blood-pressure.js
@@ -0,0 +1,30 @@
+function calculateBloodPressureResult() {
+  var systolic = document.getElementsByName("systolic")[0].value;
+  var diastolic = document.getElementsByName("diastolic")[0].value;
+  var blood_pressure = calculateBloodPressure(systolic, diastolic)
+  document.getElementById("result").innerHTML = "<div class='blood-pressure-result'>Your Blood Pressure is: " + "<div class='blood-pressure-number' style='background-color:" + blood_pressure.color + "'>" + blood_pressure.reading + "</div>" + "</div>"
+}
+
+function calculateBloodPressure(systolic_pressure, diastolic_pressure){
+  var systolic = parseInt(systolic_pressure);
+  var diastolic = parseInt(diastolic_pressure);
+
+  if(systolic >= 180 || diastolic >= 120){
+    return {reading:'Hypertensive Crisis', color:'DarkRed'};
+  }
+  else if(systolic >= 140 || diastolic >= 90){
+    return {reading: 'Stage 2 Hypertension', color: 'FireBrick'};
+  }
+  else if(systolic >= 130 || diastolic >= 80){
+    return {reading: 'Stage 1 Hypertension', color:'Crimson'};
+  }
+  else if(systolic >= 120 && diastolic >= 60){
+    return {reading: 'Elevated Blood Pressure', color: 'IndianRed'};
+  }
+  else if(systolic >= 90 || diastolic >= 60){
+    return {reading: 'Normal Blood Pressure', color: 'LightSkyBlue'};
+  }
+  else{
+    return {reading: 'Hypotension', color: 'Lavender'};
+  }
+}
