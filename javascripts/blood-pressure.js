6e1d6d0c6e3b06f4a76ebcf1dc38c5d01f37edc8
minling <mzhao725@gmail.com>
2018-04-15 20:07:43 -0400

    displays svg on result box and then shows result answer within 1300 miliseconds

--- javascripts/blood-pressure.js
+++ javascripts/blood-pressure.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("result").style.display = "none";
+  document.getElementById("result-box").style.display = "none";
+  document.getElementById("ekg_svg").style.display = "none";
 });
 
 function calculateBloodPressureResult() {
   var systolic = document.getElementsByName("systolic")[0].value;
   var diastolic = document.getElementsByName("diastolic")[0].value;
   var blood_pressure = calculateBloodPressure(systolic, diastolic);
-  document.getElementById("result").style.display = "block";
-  document.getElementById("result").innerHTML = "<div class='blood-pressure-result'>Your Blood Pressure is: " + "<div class='blood-pressure-number' style='background-color:" + blood_pressure.color + "'>" + blood_pressure.reading + "</div>" + "</div>"
+  
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("result-box").style.display = "block";
+  document.getElementById("result-box").style.height = "17%";
+
+  document.getElementById("ekg_svg").style.display = "block";
+  setTimeout(function(){ 
+    document.getElementById("ekg_svg").style.display = "none";
+    document.getElementById("result-box").style.height = "10%";
+    document.getElementById("result").innerHTML = "<div class='blood-pressure-result'>Your Blood Pressure is: " + "<div class='blood-pressure-number' style='background-color:" + blood_pressure.color + "'>" + blood_pressure.reading + "</div>" + "</div>"
+  }, 1300);
 }
 
 function calculateBloodPressure(systolic_pressure, diastolic_pressure){
